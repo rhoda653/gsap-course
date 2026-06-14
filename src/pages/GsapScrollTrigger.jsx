6621895c938683ae70831d6fe33dac549729ffc7
SMARTECH <rhodamingotamanjong@gmail.com>
2026-06-14 20:07:16 +0100

    worked ont the scrolltrigger page

--- src/pages/GsapScrollTrigger.jsx
+++ src/pages/GsapScrollTrigger.jsx
@@ -8,6 +8,15 @@ const GsapScrollTrigger = () => {
         const boxes= gsap.utils.toArray
         (scrollRef.current.children);
 
+        boxes.forEach((box) =>{
+          gsap.to(box, {
+            x: 150,
+            rotation:360,
+            borderRadius:'100%',
+            scale:1.5
+          })
+        })
+
       
     }, [])
   return (
