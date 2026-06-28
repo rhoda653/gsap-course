31da5686b30e45bf09ad83eddf9e85b9253eb488
SMARTECH <rhodamingotamanjong@gmail.com>
2026-06-28 21:14:20 +0100

    day 28 worked

--- src/pages/GsapFromTo.jsx
+++ src/pages/GsapFromTo.jsx
@@ -18,22 +18,7 @@ const GsapFromTo = () => {
         
 
         })
-        gsap.from('#green-box', {
-            rotation:0,
-            borderRadius:'0%'
-        },{
-          x: 250,
-          repeat:-1,
-          yoyo:true,
-          borderRadius:'100%',
-          rotation:360,
-          duration:2,
-          ease:"bounce.out",
-
-         
-        
-
-        })
+       
         
   }, []);
   return (
